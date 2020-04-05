845bbaca598540e98a881387a9e98a8edd8c9761
Kostyukov Kirill <kkostyukov@inbox.ru>
2020-04-05 09:43:12 +0300

    Add base.html and names in urls.py

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -20,9 +20,8 @@ from django.contrib import admin
 import mainapp.views as mainapp
 
 urlpatterns = [
-    path('', mainapp.main),
-    path('products/', mainapp.products),
-    path('contact/', mainapp.contact),
-    path('admin/', admin.site.urls),
+    path('', mainapp.main, name='main'),
+    path('products/', mainapp.products, name='products'),
+    path('contact/', mainapp.contact, name='contact'),
 ]
 
