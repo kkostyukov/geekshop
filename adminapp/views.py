beb2683f7e2b24026a270ba0ec8fcebf627a4014
Kostyukov Kirill <kkostyukov@inbox.ru>
2020-04-29 11:53:24 +0300

    HW8

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import user_passes_test
+
 from authapp.models import ShopUser
 from django.shortcuts import get_object_or_404, render
 from mainapp.models import Product, ProductCategory
@@ -6,6 +8,70 @@ from django.urls import reverse
 from authapp.forms import ShopUserRegisterForm
 from adminapp.forms import ShopUserAdminEditForm, ProductCategoryCreateForm, ProductCategoryEditForm, ProductCreateForm, \
     ProductEditForm
+from django.views.generic.list import ListView
+from django.utils.decorators import method_decorator
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.views.generic.detail import DetailView
+
+
+class UsersListView(ListView):
+    model = ShopUser
+    template_name = 'adminapp/users.html'
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+class ProductCategoryView(ListView):
+    model = ProductCategory
+    template_name = 'adminapp/categories.html'
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+class ProductCategoryCreateView(CreateView):
+    model = ProductCategory
+    template_name = 'adminapp/category_update.html'
+    success_url = reverse_lazy('admin:categories')
+    fields = '__all__'
+
+
+class ProductCategoryUpdateView(UpdateView):
+    model = ProductCategory
+    template_name = 'adminapp/category_update.html'
+    success_url = reverse_lazy('admin:categories')
+    fields = '__all__'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'категории/редактирование'
+
+        return context
+
+class ProductCategoryDeleteView(DeleteView):
+    model = ProductCategory
+    template_name = 'adminapp/category_update.html'
+    success_url = reverse_lazy('admin:categories')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.is_active = False
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+class ProductView(ListView):
+    model = Product
+    template_name = 'adminapp/products.html'
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'adminapp/product_read.html'
 
 
 def users(request):
@@ -150,42 +216,39 @@ def products(request, pk):
 
 
 def product_create(request, pk):
-    title = 'Продукты/создание'
+    title = 'продукт/создание'
+    category = get_object_or_404(ProductCategory, pk=pk)
 
     if request.method == 'POST':
-        user_form = ProductCreateForm(request.POST, request.FILES)
-        if user_form.is_valid():
-            user_form.save()
-            return HttpResponseRedirect(reverse('admin:products'))
+        product_form = ProductEditForm(request.POST, request.FILES)
+        if product_form.is_valid():
+            product_form.save()
+            return HttpResponseRedirect(reverse('admin:products', args=[pk]))
     else:
-        user_form = ProductEditForm()
+        product_form = ProductEditForm(initial={'category': category})
 
-    content = {'title': title, 'update_form': user_form}
+    content = {'title': title,
+               'update_form': product_form,
+               'category': category
+               }
 
     return render(request, 'adminapp/product_update.html', content)
 
 
 def product_read(request, pk):
-    title = 'продукты/редактирование'
-
-    edit_product = get_object_or_404(Product, pk=pk)
-    if request.method == 'POST':
-        edit_form = ProductEditForm(request.POST, request.FILES, instance=edit_product)
-        if edit_form.is_valid():
-            edit_form.save()
-            return HttpResponseRedirect(reverse('admin:product_update', args=[edit_product.pk]))
-    else:
-        edit_form = ProductEditForm(instance=edit_product)
-
-    content = {'title': title, 'update_form': edit_form}
+    title = 'продукт/подробнее'
+    product = get_object_or_404(Product, pk=pk)
+    content = {'title': title, 'object': product, }
 
     return render(request, 'adminapp/product_read.html', content)
 
 
+
 def product_update(request, pk):
-    title = 'продукты/редактирование'
+    title = 'продукт/редактирование'
 
     edit_product = get_object_or_404(Product, pk=pk)
+
     if request.method == 'POST':
         edit_form = ProductEditForm(request.POST, request.FILES, instance=edit_product)
         if edit_form.is_valid():
@@ -194,23 +257,24 @@ def product_update(request, pk):
     else:
         edit_form = ProductEditForm(instance=edit_product)
 
-    content = {'title': title, 'update_form': edit_form}
+    content = {'title': title,
+               'update_form': edit_form,
+               'category': edit_product.category
+               }
 
     return render(request, 'adminapp/product_update.html', content)
 
 
 def product_delete(request, pk):
-    title = 'продукты/удаление'
+    title = 'продукт/удаление'
 
-    category = get_object_or_404(Product, pk=pk)
+    product = get_object_or_404(Product, pk=pk)
 
     if request.method == 'POST':
-        # category.delete()
-        # вместо удаления лучше сделаем неактивным
-        category.is_active = False
-        category.save()
-        return HttpResponseRedirect(reverse('admin:categories'))
+        product.is_active = False
+        product.save()
+        return HttpResponseRedirect(reverse('admin:products', args=[product.category.pk]))
 
-    content = {'title': title, 'product_to_delete': category}
+    content = {'title': title, 'product_to_delete': product}
 
     return render(request, 'adminapp/product_delete.html', content)
